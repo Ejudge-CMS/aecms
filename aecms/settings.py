1ac6f1e0c1d5254caeadc3322577022e9402b258
Semen Kukharenko <localhost@mail.ru>
2024-01-25 20:00:50 +0300

    upd

--- aecms/settings.py
+++ aecms/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 MEDIA_URL = '/files/'
